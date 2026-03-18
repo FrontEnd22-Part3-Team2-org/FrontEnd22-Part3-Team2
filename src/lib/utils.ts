2b1c2b872ac8924a153f8978d75ce45ded8aabe8
양하늘 <skygksmf5810@gmail.com>
2026-03-18 19:17:27 +0900

    chore: cn 함수 정의 및 기타 설정

---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+/**
+ * @file cn함수 정의
+ */
+
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
