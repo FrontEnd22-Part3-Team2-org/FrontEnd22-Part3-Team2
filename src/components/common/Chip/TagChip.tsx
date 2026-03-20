9e13facd88b5b494d69861abd08e9ddfbb8c475d
YEO Sugyeong <ssyeo1204@gmail.com>
2026-03-20 18:34:16 +0900

    fix: TagChip.tsx 내 props 타입 수정

--- src/components/common/Chip/TagChip.tsx
+++ src/components/common/Chip/TagChip.tsx
@@ -7,15 +7,16 @@
 
 // TODO : [수경] 드롭다운 컴포넌트와 props 연결
 
-// interface TagChipProps {
-//   label: string;
-//   color: string;
-// }
+interface TagChipProps {
+  label: string;
+  color: string;
+  children: string;
+}
 
-// export default function TagChip({ label, color }: TagChipProps) {
-//   return (
-//     <span className={`px-2 py-1 text-xs rounded-full font-medium ${color}`}>
-//       {label}
-//     </span>
-//   );
-// }
+export default function TagChip({ children }: TagChipProps) {
+  return (
+    <span className={`px-2 py-1 text-xs rounded-full font-medium`}>
+      {children}
+    </span>
+  );
+}
