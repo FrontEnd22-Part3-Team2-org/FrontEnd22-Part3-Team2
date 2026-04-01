44290daf07580cbf6ce6f46390ef26bee0e740a2
SeungmiLee <yyaapple@gmail.com>
2026-04-02 02:29:18 +0900

    돌아가기 버튼 수정, 프로필 섹션 마진 조정

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -197,13 +197,27 @@ export default function MyPage() {
       <button
         type="button"
         onClick={() => router.back()}
-        className="mb-6 flex items-center gap-2 text-md-medium text-gray-700"
+        className="flex items-center gap-[6px] md:gap-[8px]"
       >
-        ← 돌아가기
+        <svg
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[18px] rotate-180 md:w-[20px]"
+        >
+          <path
+            d="M12.359 9.99933L6.08176 3.72208C5.91616 3.55648 5.8355 3.35937 5.8355 3.13043C5.8355 2.90149 5.91616 2.70437 6.08176 2.53878C6.24735 2.37318 6.44447 2.29252 6.67341 2.29252C6.90235 2.29252 7.09947 2.37318 7.26506 2.53878L13.9411 9.21483C14.0578 9.33153 14.1455 9.46047 14.2044 9.60164C14.2632 9.74281 14.2926 9.88838 14.2926 10.0383C14.2926 10.1882 14.2632 10.3338 14.2044 10.4749C14.1455 10.6161 14.0578 10.7451 13.9411 10.8618L7.26506 17.5378C7.09947 17.7034 6.90235 17.7841 6.67341 17.7841C6.44447 17.7841 6.24735 17.7034 6.08176 17.5378C5.91616 17.3722 5.8355 17.1751 5.8355 16.9462C5.8355 16.7172 5.91616 16.5201 6.08176 16.3545L12.359 10.0773C12.4757 9.96059 12.5346 9.82664 12.5346 9.67545C12.5346 9.52426 12.4757 9.39031 12.359 9.27361V9.99933Z"
+            fill="#333236"
+          />
+        </svg>
+
+        <span className="text-md-medium md:text-lg-medium mt-[1px] text-gray-700">
+          돌아가기
+        </span>
       </button>
 
-      <div className="flex max-w-[624px] flex-col gap-6">
-        <section className="rounded-2xl bg-white px-7 py-8">
+      <div className="flex max-w-[624px] flex-col mt-4 gap-6">
+        <section className="rounded-2xl bg-white p-6">
           <h2 className="mb-6 text-2xl-bold leading-none text-gray-900">
             프로필
           </h2>
@@ -270,7 +284,7 @@ export default function MyPage() {
           </div>
         </section>
 
-        <section className="rounded-2xl bg-white p-4">
+        <section className="rounded-2xl bg-white p-6">
           <h2 className="mb-6 text-2xl-bold leading-none text-gray-700">
             비밀번호 변경
           </h2>
